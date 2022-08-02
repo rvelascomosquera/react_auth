ef56b50c6a870f4d0de1ac0da5946a1c7708fadd
Jose <jose.zunigahenriquez@gmail.com>
2022-08-02 17:43:03 -0400

    ✨ some changes

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ function App() {
     email: "jose@larnu.com",
     discordId: "310544245155168256"
   });
-
-  const [token, setToken] = useState(null);
   const [logged, setLogged] = useState(false);
 
   const handleChange = (event) => {
@@ -23,19 +21,36 @@ function App() {
     }));
   }
 
+  async function checkToken() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      return;
+    }
+    axios.get(`${baseUrl}/auth/check`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(res => {
+      setLogged(true);
+    }).catch(err => {
+      console.log(err);
+      localStorage.removeItem('token');
+    });
+  }
+
   useEffect(()  => {
     // checkear si existe un token en el localStorage
     // si no existe, mostrar formulario de login
     // checkear si ese token es valido (endpoint check)
     // si es valido, setear logged a true
     // si no es valido, eliminar token de localStorage mostrar formulario de login
+    checkToken();
   });
 
   function login() {
     axios.post(`${baseUrl}/auth/login`, values)
       .then(res => res.data.token)
       .then(token => {
-        setToken(token);
         localStorage.setItem('token', token);
         console.log(token)
         setLogged(true);
@@ -50,10 +65,8 @@ function App() {
   return (
     <div className="App">
       {
-          logged ? <Welcome
-           token={token}
-
-          />:< LoginForm  
+          logged ? <Welcome setLogged={setLogged} />
+          : < LoginForm  
             values={values} 
             onChange={handleChange}
             submit={submit}
